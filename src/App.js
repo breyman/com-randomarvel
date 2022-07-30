abdd664245fd3e791a346ef95e5df64301f56f11
Brian Reyman <brian@brianreyman.com>
2022-07-30 09:53:29 -0600

    Basic dynamic data draw

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import myData from './data/data.json';
 
 console.log(myData[0]);
 
+const dataItem = Math.floor(Math.random() * myData.length)
+
 function App() {
 	return (
 		<div>
@@ -21,23 +23,23 @@ function App() {
 				<div className="section">
 					<div className="columns is-centered is-multiline">
 						<div className="column is-full has-text-centered">
-							<h2 className="has-text-primary is-size-2 has-text-weight-bold">{myData[1].title}</h2>
+							<h2 className="has-text-primary is-size-2 has-text-weight-bold">{myData[dataItem].title}</h2>
 						</div>
 						<div className="column is-full has-text-centered">
 							<p>
 								Watch on
 								<span className="icon ml-2 mr-2">
-									<a href={myData[1].disney} className="has-text-primary" target="_blank" rel="noreferrer">
+									<a href={myData[dataItem].disney} className="has-text-primary" target="_blank" rel="noreferrer">
 										<i className="fak fa-dplus"></i>
 									</a>
 								</span>
 								<span className="icon mr-2">
-									<a href={myData[1].amazon} className="has-text-primary" target="_blank" rel="noreferrer">
+									<a href={myData[dataItem].amazon} className="has-text-primary" target="_blank" rel="noreferrer">
 										<i className="fa-brands fa-amazon"></i>
 									</a>
 								</span>
 								<span className="icon">
-									<a href={myData[1].apple} className="has-text-primary" target="_blank" rel="noreferrer">
+									<a href={myData[dataItem].apple} className="has-text-primary" target="_blank" rel="noreferrer">
 										<i className="fa-brands fa-apple"></i>
 									</a>
 								</span>
