cbb64a8e8cf24f7b5584a33dd1e1080c7eb6ba65
Brian Reyman <brian@brianreyman.com>
2022-07-30 12:16:07 -0600

    Update Logo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 					<div className="columns is-centered is-multiline">
 						<nav className="column is-full mt-1 mb-1 is-centered has-text-centered">
 								<a href="index.html" rel="noreferrer">
-									<img src="/assets/images/logo.svg" className="is-80wide" alt="RandoMarvel Logo" />
+									<img src="/assets/images/logo.svg" className="is-256wide" alt="RandoMarvel Logo" />
 								</a>
 						</nav>
 						<div className="column is-full has-text-centered">
