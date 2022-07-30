5cd9b69f70f11fcea44a6dccefc05b1eb837ff0e
Brian Reyman <brian@brianreyman.com>
2022-07-30 09:46:27 -0600

    Update Footer to Behave Properly

--- src/App.js
+++ src/App.js
@@ -49,16 +49,18 @@ function App() {
 			<footer className="footer has-text-centered">
 				<div clasNames="container">
 					<div className="columns is-centered is-size-7">
-						<div className="mr-3">
-							<a href="index.html" className="has-text-primary">
-								Pick a Movie
-							</a>
-						</div>
-						<div>|</div>
-						<div className="ml-3">
-							<a href="index.html" className="has-text-primary">
-								About
-							</a>
+						<div className="column is-full is-centered has-text-centered">
+							<span className="mr-3">
+								<a href="index.html" className="has-text-primary">
+									Pick a Movie
+								</a>
+							</span>
+							<span className="has-text-grey">|</span>
+							<span className="ml-3">
+								<a href="index.html" className="has-text-primary">
+									About
+								</a>
+							</span>
 						</div>
 					</div>
 				</div>
