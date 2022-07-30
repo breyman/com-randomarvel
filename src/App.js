27dfec570eb2652d5dcf77814c9a303d1903285a
Brian Reyman <brian@brianreyman.com>
2022-07-29 21:29:26 -0600

    Add SASS, Bulma and Some Testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.sass';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>RandoMarvel</h1>
+		<div>
+			<header>
+				<h1 className="has-text-primary">RandoMarvel</h1>
 				<p>
-					Edit <code>src/App.js</code> and save to reload. Brian
+					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ root.render(
 	</React.StrictMode>
 );
 
+// ReactDOM.createRoot(document.getElementById('second-root')).render(<h1>Brian Wins</h1>);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
