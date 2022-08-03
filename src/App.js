fbf5e503b63d7ccae9856f8388812685244703c5
Brian Reyman <brian@brianreyman.com>
2022-08-02 20:07:51 -0600

    Add Google Analytics Call for Home Page Load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './assets/styles/styles.sass';
 import Page from './components/Page';
 
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-27W2KXSBSN");
+
+ReactGA.event({category:"page", action:"load", label:"home"});
+
 function App() {
 
 	return (
