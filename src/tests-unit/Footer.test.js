eccdd2edcb6becf9716e1087327c77d8a39409a5
Brian Reyman <brian@brianreyman.com>
2022-10-01 06:48:55 -0600

    Write a unit test for footer error display

--- src/tests-unit/Footer.test.js
+++ src/tests-unit/Footer.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import Footer from '../components/Footer';
 
-beforeEach(() => {
-    render(<Footer handlePageChange={event => setPage(event.target.title)} />);
-  });
-
 // test for the correct icon class, color and link
 it('displays footer links', () => {
-    const generateElement = screen.getByText(/Generate Random Marvel/i);
-    const listElement = screen.getByText(/Marvel List/i);
-    const aboutElement = screen.getByText(/About/i);
-    expect(generateElement).toBeInTheDocument();
-    expect(listElement).toBeInTheDocument();
-    expect(aboutElement).toBeInTheDocument();
+  render(<Footer handlePageChange={event => setPage(event.target.title)} />);
+
+  const generateElement = screen.getByText(/Generate Random Marvel/i);
+  const listElement = screen.getByText(/Marvel List/i);
+  const aboutElement = screen.getByText(/About/i);
+  expect(generateElement).toBeInTheDocument();
+  expect(listElement).toBeInTheDocument();
+  expect(aboutElement).toBeInTheDocument();
+});
+
+// test for displaying warning if local data file was used
+it('displays footer data file error', () => {
+  render(<Footer handlePageChange={event => setPage(event.target.title)} usingLocalDataFile={true} />);
+  
+  const errorElement = screen.getByText(/local data/i);
+  expect(errorElement).toBeInTheDocument();
 });
