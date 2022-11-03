e02ec5fbe503aa7641d29a3370d82ffd4871edb0
Brian Reyman <brian@brianreyman.com>
2022-11-02 19:13:45 -0600

    Initial Split.io install and config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// CDN will expose the library globally with the "splitio" variable. Use the options that work best with your code.
+const { SplitFactory } = require("@splitsoftware/splitio-react");
+// Create the config for the SDK factory.
+const sdkConfig = {
+  core: {
+    authorizationKey: "ga81m1op6iucrm8qngngitadmjogekp8tek3",
+    key: "key",
+  },
+};
+
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <SplitFactory config={sdkConfig}>
+      <App />
+    </SplitFactory>
+  </React.StrictMode>
 );
-
-// ReactDOM.createRoot(document.getElementById('second-root')).render(<h1>Brian Wins</h1>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
